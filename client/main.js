fac023e64b8f8694e2c0094fa986e2ee4c6f9cfb
Muhammad Hamza <muhammadhamza14210@gmail.com>
2022-12-30 15:51:56 -0700

    initial commit

---
+++ client/main.js
@@ -0,0 +1,91 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    
+    if (element.textContent === '....'){
+      element.textContent = '';
+    } 
+  }, 300)
+}
+
+function typeText(element,text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }else{
+        clearInterval(interval);
+    }
+  },20)
+}
+
+
+//Unique ID for each message so its easy to map
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+//Chatstripe is used to change colors for the user depending on AI or user
+function chatStripe (isAi, value, uniqueId){
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class = 'chat'>
+          <div class = 'profile'>
+            <img
+              src = "${isAi ? bot:user}"
+              alt = "${isAi ? 'bot':'user'}"
+              />
+          </div> 
+          <div class='message' id = ${uniqueId}> ${value}</div>
+        </div> 
+
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async(e) => {
+  e.preventDefault()
+
+  const data = new FormData(form);
+
+  //user's stripe
+  chatContainer.innerHTML += chatStripe(false,data.get('prompt'));
+
+  form.reset();
+
+  //bot's stripe
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true," ",uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+}
+
+form.addEventListener('submit',handleSubmit);
+form.addEventListener('keyup',(e) => {
+  if(e.keyCode === 13){
+    handleSubmit(e);
+  }
+})
