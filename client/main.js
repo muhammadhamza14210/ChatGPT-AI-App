ab4ab26d541c77261825401bac570ba3d8ec00ab
Muhammad Hamza <muhammadhamza14210@gmail.com>
2022-12-30 17:50:12 -0700

    final commit

--- client/main.js
+++ client/main.js
@@ -82,7 +82,7 @@ const handleSubmit = async(e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://codex-j1ob.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type': 'application/json',
